b9c7b4f70b54be0b3f08bb42ca865a88fc9f00b0
riboha <68260093+riboha@users.noreply.github.com>
2023-01-29 21:52:16 +0900

    Time: 84 ms (78.57%), Space: 50.2 MB (50.00%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.js
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var maximumXOR = function(nums) {
-  let answer = nums.reduce(
+  return nums.reduce(
     function(accumulator, currentValue) {
-      console.log(accumulator, currentValue)
       return accumulator |= currentValue;
     }, 
   0);
 
-  return answer;    
 };
