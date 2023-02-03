cf72b8d300561969db02b487651e66c52a473345
riboha <68260093+riboha@users.noreply.github.com>
2023-02-03 17:50:47 +0900

    Time: 74 ms (52.24%), Space: 42.1 MB (43.28%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -4,11 +4,10 @@
  * @return {number}
  */
 var timeRequiredToBuy = function(tickets, k) {
-    let timeArray = Array(tickets.length).fill(0);
     let answer = 0;
-    while (tickets.length) {
-        for (let idx = 0; idx < tickets.length; idx ++) {
-            if (tickets[idx] !== 0)  {
+    while (true) {
+        for (let idx = 0, len = tickets.length; idx < len; idx ++) {
+            if (tickets[idx])  {
                 if (tickets[idx] === 1 && idx === k) {
                     return ++answer;
                 }
